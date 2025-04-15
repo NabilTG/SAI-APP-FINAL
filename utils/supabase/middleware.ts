f72167b5425acba941848edaaa20bbf7c7b4c231
NabilTG <131936159+NabilTG@users.noreply.github.com>
2025-04-15 16:40:05 -0600

    cambios mejoras

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -139,3 +139,8 @@ export const updateSession = async (request: NextRequest) => {
     });
   }
 };
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  runtime: "nodejs",
+};
